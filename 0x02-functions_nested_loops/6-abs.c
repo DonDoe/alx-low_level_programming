5a33193b7854d78fe75239f5ef31b5258f8f7f13
Steve Donell <stevedonell27@gmail.com>
2023-06-20 07:43:10 -0700

    fsysgt

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_abs - computes absolute values of integers
+ *@c: the number under computation
+ *Return: zero or absolute number of a value
+ */
+
+int _abs(int c)
+
+{
+	if (c < 0)
+	{
+	int abs_val;
+
+	abs_val = c * -1;
+	return (abs_val);
+	}
+	return (c);
+}
