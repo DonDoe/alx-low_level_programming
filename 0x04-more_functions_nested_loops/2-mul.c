d708dff4edcd964df5168a9c26e9be68d8a72a66
Steve Donell <stevedonell27@gmail.com>
2023-06-22 11:42:51 -0700

    hjsjsj

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul: multiplies the two int
+ * @a: 1st int
+ * @b: 2nd int
+ * return: always 0
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+	return (result);
+}
