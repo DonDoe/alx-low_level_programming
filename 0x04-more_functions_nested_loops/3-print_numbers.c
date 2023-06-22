fc5f2aa3511fed9372c8fbfead09b5ac9f03bbee
Steve Donell <stevedonell27@gmail.com>
2023-06-22 11:50:20 -0700

    hshsj

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers: prints num from 0-9
+ * return: 0
+ */
+
+void print_numbers(void)
+{
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
