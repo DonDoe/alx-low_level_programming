f56f7ecb11d78ebe18ecc6e9ebce62f1c998f2ec
Steve Donell <stevedonell27@gmail.com>
2023-06-26 07:12:51 -0700

    jshs

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,15 @@
 #include "main.h"
 #include <stdio.h>
-/** puts2: prints every other character of a string, starting with the first character
+/**
+ *puts2: prints every other character of a string, starting with the first character
  *@str - the sdtring reference
  *return- 0
  */
 void puts2(char *str)
 {
 	int z = 0;
-
-	while (str[z] != '\0')
-	{
-		if (z % 2 == 0)
-		{
+	while (str[z] != '\0')	{
+		if (z % 2 == 0) {
 			putchar(str[z]);
 		}
 		z++;
